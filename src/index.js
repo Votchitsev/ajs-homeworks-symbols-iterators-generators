728f1df4c83ac67d0260f3c62c38817f010acb1d
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-07-04 15:00:05 +0200

    task 2 complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import Team from './js/task1/Team.js';
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable import/extensions */
+import Team from './js/task1/Team(with iterator).js';
+import TeamWithGenerator from './js/task1/Team(with generator).js';
 import Character from './js/task1/Character.js';
 
 const daemon = new Character('Dima', 'Daemon', 100, 1, 20, 50);
@@ -6,12 +9,21 @@ const magician = new Character('Ivan', 'magocian', 80, 2, 25, 60);
 const archer = new Character('Vasia', 'archer', 50, 2, 90, 60);
 
 const team = new Team();
-
 team.addAll(daemon, magician, archer);
 
-// TASK 2 RESULT
+const teamTwo = new TeamWithGenerator();
+teamTwo.addAll(daemon, magician, archer);
 
-// eslint-disable-next-line no-restricted-syntax
+// TASK 1 RESULT
+console.log('TASK ONE');
+
 for (const i of team) {
+  console.log(i);
+}
+
+// TASK 2 RESULT
+console.log('TASK TWO');
+
+for (const i of teamTwo) {
   console.log(i);
 }
